831f0e52e778cab7bb0816019de314502bbc7295
mendoncp <pramodmendonca@Pramods-MacBook-Air.local>
2023-10-02 15:15:57 -0400

    feat: insitial commit-3

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import React from "react";
 const App = () =>{
     return (
         <h1>
-            Welccome Pramod Mendonca !!!
+            Welccome Pramod Mendonca to react !!!
         </h1>
     )
 }
